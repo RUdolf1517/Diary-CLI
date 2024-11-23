9e964222b7c06cab71159296ea98e47a0379131f
Rudolf1517 <mermerovich@mail.ru>
2024-11-23 20:52:57 +0300

    add lib and hello function

---
+++ lib_for_program.py
@@ -0,0 +1,3 @@
+def start_message():
+    m1 = print(f"1. Add new task\n2. Delete task\n3. See tasks\n4. Exit ")
+    m = input("choose the comand:")
---
+++ main.py
@@ -0,0 +1,6 @@
+from lib_for_program import *
+def motor():
+    while True:
+        start_message()
+
+motor()
