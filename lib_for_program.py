f3cb0a1916cd665dd6479e60d59676b32fd4f80f
Rudolf1517 <mermerovich@mail.ru>
2024-11-23 21:05:17 +0300

    add menu

--- lib_for_program.py
+++ lib_for_program.py
@@ -1,3 +1,16 @@
+import sys
 def start_message():
     m1 = print(f"1. Add new task\n2. Delete task\n3. See tasks\n4. Exit ")
-    m = input("choose the comand:")
+
+def choose(m):
+    if m == 1:
+        pass
+    elif m == 2:
+        pass
+    elif m == 3:
+        pass
+    elif m == 4:
+        print("Goodbye!")
+        sys.exit()
+    else:
+        print("Idk, pls repeate")
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from lib_for_program import *
 def motor():
     while True:
         start_message()
-
+        m = int(input("choose the comand:"))
+        choose(m)
 motor()
