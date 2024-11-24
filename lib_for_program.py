136363b905cf35bad8d3f08a712fcf662680400b
Rudolf1517 <mermerovich@mail.ru>
2024-11-24 20:15:38 +0300

    add json file and make code prettier

--- lib_for_program.py
+++ lib_for_program.py
@@ -1,16 +1,49 @@
 import sys
+import json
+from tabulate import tabulate
+
+
 def start_message():
-    m1 = print(f"1. Add new task\n2. Delete task\n3. See tasks\n4. Exit ")
+    m1 = print(f"1. Add new task\n2. See tasks\n3. Delete task\n4. Exit ")
+
 
 def choose(m):
     if m == 1:
-        pass
+        make_new_task()
     elif m == 2:
-        pass
+        read_new_tasks()
     elif m == 3:
-        pass
+        delete_new_tasks()
     elif m == 4:
-        print("Goodbye!")
-        sys.exit()
+        exit()
     else:
         print("Idk, pls repeate")
+
+
+
+def make_new_task():
+    with open("tasks.json", mode="r+", encoding="utf-8") as file:
+        Tasks_base = {"Tasks:": {"beta": ""}}
+        Complete_base = {"Complete": {"beta": ""}}
+        Description_base = {"Description": {"beta": ""}}
+        tasks_new = ""
+        complete_new = ""
+        description_new = ""
+
+        data = json.load(file)
+
+
+def read_new_tasks():
+    with open("tasks.json", mode="r", encoding="utf-8") as file:
+        data = json.load(file)
+        print(tabulate(data, headers="keys", tablefmt="grid"))
+        print(data)
+
+
+def delete_new_tasks():
+    pass
+
+
+def exit():
+    print("Goodbye!")
+    sys.exit()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from lib_for_program import *
+
+
 def motor():
     while True:
         start_message()
         m = int(input("choose the comand:"))
         choose(m)
+
+
 motor()
